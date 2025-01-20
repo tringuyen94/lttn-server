f5dc46f4f6afe25ae7b74fb1ce79d7795ae30de3
Tri Nguyen <tringuyen194.dev@gmail.com>
2025-01-20 16:35:53 +0700

    add count-visitor

--- middlewares/count-visitor.js
+++ middlewares/count-visitor.js
@@ -5,7 +5,6 @@ module.exports = async (req, res, next) => {
     let counter = await Counter.findOne();
     counter.visitors += 1; // Tăng số lượt truy cập
     await counter.save(); // Lưu lại số lượt truy cập vào MongoDB
-    req.visitors = counter.visitors;
     next();
   } catch (err) {
     next();
