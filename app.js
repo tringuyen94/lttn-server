3924cd8736882f86ae2d5aa858a93a1ae0b67677
Tri Nguyen <tringuyen194.dev@gmail.com>
2025-01-20 16:34:30 +0700

    add cors count-visitor cookie-parser turn off swagger

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 const appApi = require('./routes/api');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('./swagger.json');
+const cookieParser = require('cookie-parser');
 const Database = require('./database');
 const errorHandler = require('./error-handler');
 const { NotFoundError } = require('./response/error.response');
@@ -17,15 +16,29 @@ const countVisitor = require('./middlewares/count-visitor');
 Database.getInstance();
 
 /**
- * COUNT VISITORS
- */
-app.use(countVisitor);
-
-/**
  * GLOBAL Middlewares
  */
-app.use(cors());
-app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Your frontend's URL
+    credentials: true,
+    methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", 'cdnjs.cloudflare.com'], // Allow external script sources
+      imgSrc: ["'self'", 'your-image-domain.com'], // Allow image domain
+      styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles
+    },
+  })
+);
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
 /**
  * BODY PARSER
  */
@@ -34,6 +47,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 //
+/**
+ * COUNT VISITORS
+ */
+app.use(countVisitor);
 
 // API
 app.use('/api/v1', appApi);
